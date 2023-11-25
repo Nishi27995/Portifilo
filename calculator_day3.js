8cd064886262f5a10ea388299928e91561b58ea5
Nishi27995 <nishi.singh@msds.christuniversity.in>
2023-11-25 11:33:40 +0530

    index.html

---
+++ calculator_day3.js
@@ -0,0 +1,19 @@
+function equation(val){
+    document.getElementById("text1").value+=val;
+
+    
+}
+
+function calculator(val){
+    var a= document.getElementById("text1").value
+    var c=eval(a)
+document.getElementById("text1").value;
+
+}
+
+function clear(val){
+    document.getElementById("text").value=" ";
+    
+
+
+}
